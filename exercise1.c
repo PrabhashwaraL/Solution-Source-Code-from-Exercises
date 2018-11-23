e6307c878222da2553a6b550723092b1c490ee79
Lankesh Prabhashwara <lankeshprabhashwara@gmail.com>
2018-11-23 22:45:23 +0530

    Add exercise1.c

---
+++ exercise1.c
@@ -0,0 +1,13 @@
+// Exercise 1
+// A C program that print name, DOB and mobile number
+#include <stdio.h>
+
+// function main begins program execution
+int main(void)
+{
+    printf("Name\t: John Doe\n");
+    printf("DOB\t: January 01, 1997\n");
+    printf("Mobile\t: +9499-9999999\n");
+
+    return 0;
+} // end function main
