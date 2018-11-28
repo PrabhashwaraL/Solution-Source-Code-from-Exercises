4e576b74ac2351cad3827a1723190875b3c7f638
Lankesh Prabhashwara <lankeshprabhashwara@gmail.com>
2018-11-28 12:29:52 +0530

    update exercise14.c
    Apply some conding standards.

--- exercise14.c
+++ exercise14.c
@@ -13,13 +13,13 @@ int main( void )
     float fuel;
 
     // prompt user for input and obtain values
-    printf("Input total distance in km: ");
-    scanf("%f", &distance);
-    printf("Input total fuel spent in liters: ");
-    scanf("%f", &fuel);
+    printf( "Input total distance in km: " );
+    scanf( "%f", &distance );
+    printf( "Input total fuel spent in liters: " );
+    scanf( "%f", &fuel );
 
     // calculate and display result with 3 decimal places
-    printf("Average consumption(km/lt): %.3f\n", distance / fuel);
+    printf( "Average consumption(km/lt): %.3f\n", distance / fuel );
 
     return 0;
 } // end function main
